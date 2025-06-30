46151f0b45d0a3d2b631cfa2c14b353f0a7ce662
yhuet <yann@huet.dev>
2025-06-30 14:22:41 +0200

    gemini

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+content = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(content.text)
+print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {content.usage_metadata.candidates_token_count }")
