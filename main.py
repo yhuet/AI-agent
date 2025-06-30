a556a7d3a5e50aaaff8a6244177c7cc99b217626
yhuet <yann@huet.dev>
2025-06-30 14:43:57 +0200

    messages list

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-content = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(content.text)
-print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {content.usage_metadata.candidates_token_count }")
+if len(sys.argv) < 2:
+    print("error: missing prompt")
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", 
+    contents=messages
+)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
