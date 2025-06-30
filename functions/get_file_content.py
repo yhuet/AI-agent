5ffc08a27e2935d0c2646796cebc8c8a349de4e1
yhuet <yann@huet.dev>
2025-06-30 18:59:24 +0200

    get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,18 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    absolute_working_directory = os.path.abspath(working_directory)
+    absolute_full_path = os.path.abspath(full_path)
+    if not absolute_full_path.startswith(absolute_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(full_path, "r") as file:
+            content = file.read(10000)
+            if len(content) > 9999:
+                content = content[:10000] + f'\n[...File "{file_path}" truncated at 10000 characters]'
+            return content
+    except Exception as e:
+        print(f"Error: {e}")
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def main():
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
+    
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat")) # (this should return an error string)
+
 
 if __name__ == "__main__":
     main()
