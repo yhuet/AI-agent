a7d0fe1c836bbefa05357cb0fda07dc40e5c895a
yhuet <yann@huet.dev>
2025-07-01 11:44:26 +0200

    system prompt

--- main.py
+++ main.py
@@ -22,12 +22,15 @@ args = parser.parse_args()
 user_prompt = args.prompt
 verbose = args.verbose
 
+model_name = "gemini-2.0-flash-001"
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+system_prompt="Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", 
-    contents=messages
+    model=model_name, 
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt)
 )
 print(response.text)
 if verbose:
