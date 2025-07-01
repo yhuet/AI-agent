f5337ee72a052812f734dcfa8d765b88de006767
yhuet <yann@huet.dev>
2025-07-01 14:11:18 +0200

    calling functions

---
+++ call_function.py
@@ -0,0 +1,41 @@
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+from google.genai import types
+
+
+def call_function(function_call_part, verbose=False):
+    functions = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    if function_call_part.name in functions:
+        func = functions[function_call_part.name]
+        function_result = func(working_directory="./calculator", **function_call_part.args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    
--- main.py
+++ main.py
@@ -8,62 +8,78 @@ from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.run_python_file import schema_run_python_file
 from functions.write_file import schema_write_file
+from call_function import call_function
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2:
-    print("error: missing prompt")
-    sys.exit(1)
+    if len(sys.argv) < 2:
+        print("error: missing prompt")
+        sys.exit(1)
 
-parser = argparse.ArgumentParser(description='AI Agent CLI')
-parser.add_argument('prompt', help='The user prompt')
-parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+    parser = argparse.ArgumentParser(description='AI Agent CLI')
+    parser.add_argument('prompt', help='The user prompt')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-user_prompt = args.prompt
-verbose = args.verbose
+    user_prompt = args.prompt
+    verbose = args.verbose
 
-model_name = "gemini-2.0-flash-001"
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
-        schema_write_file,
+    model_name = "gemini-2.0-flash-001"
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-)
-system_prompt = """
-You are a helpful AI coding agent.
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    generate_content(client, messages, verbose)
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-response = client.models.generate_content(
-    model=model_name, 
-    contents=messages,
-    config=types.GenerateContentConfig(
-        tools=[available_functions],
-        system_instruction=system_prompt
+def generate_content(client, messages, verbose):
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
+        ]
     )
-)
-if response.function_calls:
-    for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-else:
-    print(response.text)
-if verbose:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    response = client.models.generate_content(
+        model=model_name, 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
+    )
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            call_result = call_function(function_call_part)
+            if not call_result.parts or not call_result.parts[0].function_response.response:
+                raise Exception("Fatal: function call ressult empty")
+            if verbose:
+                print(f"-> {call_result.parts[0].function_response.response}")
+
+    else:
+        print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
+
+
+if __name__ == "__main__":
+    main()
